23f29196259b26317b9bf37733c4f01148dca2c3
Abhijith <abhijith.142@gmail.com>
2017-08-08 21:09:05 +0530

    mongodb

--- src/main/java/org/learnings/springBoot/login/LoginController.java
+++ src/main/java/org/learnings/springBoot/login/LoginController.java
@@ -7,6 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
+	
+	@RequestMapping(method=RequestMethod.GET,value="/signUp")
+	public String signUpForm()
+	{
+		return "signUp";
+	}
+	
 	@RequestMapping(method=RequestMethod.POST,value="/signUp")
 	public void signUp(@ModelAttribute UserFormBean userFormBean)
 	{
