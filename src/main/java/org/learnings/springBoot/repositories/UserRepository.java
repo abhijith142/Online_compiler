7b90d37e0a9dd5d303afd77f6b85a82a5f185251
Abhijith <abhijith.142@gmail.com>
2017-08-23 10:50:26 +0530

    Insertion working

--- src/main/java/org/learnings/springBoot/repositories/UserRepository.java
+++ src/main/java/org/learnings/springBoot/repositories/UserRepository.java
@@ -3,6 +3,6 @@ package org.learnings.springBoot.repositories;
 import org.learnings.springBoot.dto.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface userRepository extends MongoRepository<User, Integer>{
+public interface UserRepository extends MongoRepository<User, Integer>{
 	public User findByName(String name);
 }
--- src/main/java/org/learnings/springBoot/service/UserService.java
+++ src/main/java/org/learnings/springBoot/service/UserService.java
@@ -1,13 +1,16 @@
 package org.learnings.springBoot.service;
 
 import org.learnings.springBoot.dto.User;
+import org.learnings.springBoot.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-	
+	@Autowired
+	UserRepository userRepository;
 	
 	public void signUp(User user) {
-		
+		userRepository.save(user);
 	}
 }
